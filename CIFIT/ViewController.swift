3bb0692a6a3bdd124ae561d02874c1484f06f00a
Namik Catibusic <namik.catibusic@gmail.com>
2017-12-18 13:08:04 +0100

    Change to swift

---
+++ CIFIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIFIT
+//
+//  Created by Namik Catibusic on 18/12/2017.
+//  Copyright © 2017 Namik Catibusic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
