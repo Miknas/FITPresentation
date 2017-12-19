56359df82e1441527374cea9958ddfcecf341f49
Namik Catibusic <namik.catibusic@gmail.com>
2017-12-19 23:44:14 +0100

    Push for tests

--- CIFITTests/CIFITTests.swift
+++ CIFITTests/CIFITTests.swift
@@ -24,13 +24,8 @@ class CIFITTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert("test" == "test")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+   
 }
